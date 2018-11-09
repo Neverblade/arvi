a863747fe6100c0311e92550bcc57fdca6075e0f
FengMengshi <MengshiFeng@FengMengshis-MacBook-Pro.local>
2018-11-08 19:11:32 -0800

    Link UI for load and localize

---
+++ Assets/Scripts/Modes/LocalizeMode.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using MM = ModeManager;
+
+public class LocalizeMode : Mode
+{
+    public Mode mapListMode;
+
+    public override void CleanupMode()
+    {
+        // Clean up elements
+        MM.instance.elements.Clear();
+
+        // Clean up event handlers
+        TapSwipeDetector.OnTap -= OutputCurrentElement;
+        TapSwipeDetector.OnDoubleTap -= OnDoubleTapDefault;
+    }
+
+    public override void SetupMode()
+    {
+        // Set up elements
+        List<MM.Element> elements = new List<MM.Element>();
+        elements.Add(new MM.Element("Cancel", OnSelectCancel));
+        MM.instance.elements = elements;
+        MM.instance.index = 0;
+
+        // Set up event handlers
+        TapSwipeDetector.OnTap += OutputCurrentElement;
+        TapSwipeDetector.OnDoubleTap += OnDoubleTapDefault;
+
+        // Output current element name
+        OutputCurrentElement();
+    }
+
+    public void OnSelectCancel()
+    {
+        Debug.Log("Cancelling. Moving to MainList Mode.");
+        MM.instance.SwitchModes(mapListMode);
+    }
+
+}
---
+++ Assets/Scripts/Modes/MapListMode.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MM = ModeManager;
+
+public class MapListMode : Mode
+{
+    public GameObject mapListPanel;
+    public Mode localizeMode;
+    public Mode mainMenuMode;
+
+    public override void CleanupMode()
+    {
+        mapListPanel.SetActive(false);
+
+        // Clean up elements
+        MM.instance.elements.Clear();
+
+        // Clean up event handler
+        TapSwipeDetector.OnSwipe -= OnSwipeDefault;
+        TapSwipeDetector.OnTap -= OutputCurrentElement;
+        TapSwipeDetector.OnDoubleTap -= OnDoubleTapDefault;
+    }
+
+    public override void SetupMode()
+    {
+        mapListPanel.SetActive(true);
+
+        // Set up elements
+        List<MM.Element> elements = new List<MM.Element>();
+        elements.Add(new MM.Element("load map list", OnSelectMapList));
+        elements.Add(new MM.Element("Cancel", OnSelectCancel));
+        MM.instance.elements = elements;
+        MM.instance.index = 0;
+
+        // Set up event handlers
+        TapSwipeDetector.OnSwipe += OnSwipeDefault;  //need to override to implement vertical swipe when connect to placenote 
+        TapSwipeDetector.OnTap += OutputCurrentElement;
+        TapSwipeDetector.OnDoubleTap += OnDoubleTapDefault;
+
+        // Output current element name
+        OutputCurrentElement();
+    }
+
+    public void OnSelectMapList()
+    {
+        Debug.Log("Selecting map. Moving to Localize Mode.");
+        MM.instance.SwitchModes(localizeMode);
+    }
+
+    public void OnSelectCancel()
+    {
+        Debug.Log("Cancelling. Moving to Main Menu Mode.");
+        MM.instance.SwitchModes(mainMenuMode);
+    }
+
+}
